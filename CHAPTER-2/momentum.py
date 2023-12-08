31db0101e4d0697e50c5a3bd0c2de926c1c2cecc
PeterWanjihia <peter.wanjihia@student.moringaschool.com>
2023-12-09 00:23:57 +0300

    Added the change to view the kinetic energy

--- CHAPTER-2/momentum.py
+++ CHAPTER-2/momentum.py
@@ -11,7 +11,9 @@ velocity = float (input('Enter the velocity of body:'))
 
 momentum = mass * velocity
 
+kEnergy  = 1/2 * mass * velocity ** 2
+
 def fmomentum(x,y):
-    return (f'The momentum of this body is {momentum:.2f}')
+    return (f'The momentum of this body is {momentum:.2f}\n While the momentum is {kEnergy:.2f}')
 
 print(fmomentum(mass,velocity))
