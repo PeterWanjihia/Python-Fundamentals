59f2236b60d92551ea5ad66a42ba720908dd916b
PeterWanjihia <peter.wanjihia@student.moringaschool.com>
2023-12-09 13:21:22 +0300

    This code allows inputs of sides of triangles and checks whether the dimensions fit the rightangle or not

---
+++ CHAPTER-3-ControlFlow/right_angle.py
@@ -0,0 +1,12 @@
+
+hypotenuse = float(input('Enter the length of  hypotenuse:'))
+adjacent = float(input('Enter the length of  adjacent:'))
+base = float(input('Enter the length of  base:'))
+
+
+if  (hypotenuse ** 2  == (adjacent ** 2 + base ** 2)):
+    print('This is a right angled triangle')
+else:
+    print('This is not a right angled triangle')
+    
+
